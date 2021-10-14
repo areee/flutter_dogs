38ca226b6959b7102f5dd8135d90c66e408dfc2e
areee <10089872+areee@users.noreply.github.com>
2021-10-14 22:27:32 +0300

    Update main to use the data model in cards & go to the detail view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dog.dart';
+import 'dog_detail.dart';
 
 void main() {
   runApp(const DogApp());
@@ -50,7 +53,47 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Container(),
+        child: ListView.builder(
+          itemCount: Dog.finnishBreeds.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return DogDetail(dog: Dog.finnishBreeds[index]);
+                    },
+                  ),
+                );
+              },
+              child: buildDogCard(Dog.finnishBreeds[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildDogCard(Dog dog) {
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(dog.imageUrl)),
+            const SizedBox(height: 21.0),
+            Text(
+              dog.breedName,
+              style: GoogleFonts.andada(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
