2f39b41e6e9510d23631fcb850e38526e6a1450f
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2021-10-24 22:44:24 +0300

    Enhance UI for part 1 (#6)
    * Move MyHomePage -> own file ('home_page.dart')
    
    * Add imageSource & imageCopyright into dog.dart
    
    * Some updates to dog model
    
    * Add  'date' & update 'dogType' for the dog model
    
    * Auto-update pubspec.lock & macOS-related files
    
    * Update home_page & dog_detail to have better UI (adapt from book club repo)

--- lib/dog_detail.dart
+++ lib/dog_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dog.dart';
+import 'package:intl/intl.dart';
 
 class DogDetail extends StatefulWidget {
   final Dog dog;
@@ -17,62 +18,126 @@ class DogDetail extends StatefulWidget {
 }
 
 class _DogDetailState extends State<DogDetail> {
-  double _sliderVal = 14.0;
+  double _sliderScalar = 1.0;
+  final TransformationController _controller = TransformationController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.dog.breedName),
+        title: Text(widget.dog.breedName!),
       ),
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Image(
-                image: AssetImage(widget.dog.imageUrl),
+        child: Stack(
+          children: [
+            Positioned(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width,
+              child: InteractiveViewer(
+                transformationController: _controller,
+                child: Image(
+                  image: AssetImage(widget.dog.imageUrl!),
+                  /* Currently fitHeight is better, 
+                  otherwise fitWidth may be better */
+                  fit: BoxFit.fitHeight,
+                ),
               ),
             ),
-            const SizedBox(
-              height: 4,
-            ),
-            Text(
-              widget.dog.breedName,
-              style: GoogleFonts.montserrat(fontSize: 18),
-            ),
-            Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(7.0),
-                itemCount: widget.dog.registrationQuantities.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final registration = widget.dog.registrationQuantities[index];
-                  return Text(
-                    'Year ${registration.year}: ${registration.quantity} dogs',
-                    style: TextStyle(
-                      fontSize: _sliderVal,
-                    ),
-                  );
-                },
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.4,
+              height: MediaQuery.of(context).size.height * 0.6 - 50,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                children: [
+                  Slider(
+                    min: 1.0,
+                    max: 4.0,
+                    divisions: 20,
+                    label: _sliderScalar.toStringAsPrecision(2),
+                    value: _sliderScalar.toDouble(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _sliderScalar = newValue;
+                        _controller.value =
+                            Matrix4.identity().scaled(_sliderScalar);
+                      });
+                    },
+                    activeColor: Colors.lightGreen,
+                    inactiveColor: Colors.black,
+                  ),
+                  _DogBody(widget.dog, scalar: _sliderScalar),
+                ],
               ),
-            ),
-            Slider(
-              min: 14.0,
-              max: 24.0,
-              divisions: 10,
-              label: 'Font size $_sliderVal',
-              value: _sliderVal,
-              onChanged: (newValue) {
-                setState(() {
-                  _sliderVal = newValue;
-                });
-              },
-              activeColor: Colors.lightGreen,
-              inactiveColor: Colors.black,
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _DogBody extends StatelessWidget {
+  const _DogBody(this.dog, {required this.scalar});
+  final Dog dog;
+  final double scalar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                DateFormat('dd.MM.yyyy').format(dog.date!),
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Chip(
+                backgroundColor: Colors.lightGreen,
+                label: Text(
+                  'Zoom: ${scalar.toStringAsPrecision(2)}',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1!
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 4),
+          Text(
+            dog.breedName!,
+            style: GoogleFonts.montserrat(fontSize: 18),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            'Copyright: ${dog.imageCopyright!}',
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            dog.moreInfo!,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Text source: ${dog.moreInfoUrl!}'
+            '\n Image source: ${dog.imageSource!}',
+            style: const TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dogs/dog.dart';
+import 'package:flutter_dogs/dog_detail.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Dog.finnishBreeds.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return DogDetail(dog: Dog.finnishBreeds[index]);
+                    },
+                  ),
+                );
+              },
+              child: _buildDogCard(Dog.finnishBreeds[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDogCard(Dog dog) {
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(dog.imageUrl!)),
+            const SizedBox(height: 10.0),
+            Text(
+              DateFormat('dd.MM.yyyy').format(dog.date!),
+              style: const TextStyle(fontSize: 14.0),
+            ),
+            const SizedBox(height: 5.0),
+            Text(
+              dog.breedName!,
+              style: GoogleFonts.andada(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
